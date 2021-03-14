25cdba86f90d9c0c88a3630f143c5d95374b078e
ashis <ashis@192.168.1.157>
2021-03-14 14:22:30 +0100

    First commit for Swift Alliance Access Tool for MQ

---
+++ SwiftAllianceMQloader/src/main/java/com/swift/alliance/tool/mq/MQloader_main.java
@@ -0,0 +1,10 @@
+package com.swift.alliance.tool.mq;
+
+public class MQloader_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SwiftAllianceMQloader/src/main/java/com/swift/alliance/tool/mq/package-info.java
@@ -0,0 +1,1 @@
+package com.swift.alliance.tool.mq;
